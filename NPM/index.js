1a0c152e63470601912250b9fae199be35991965
Rasul Yakub Panari <panarirasul@gmail.com>
2024-08-17 01:07:14 +0530

    npm project

---
+++ NPM/index.js
@@ -0,0 +1,15 @@
+//var generateName = require("sillyname");
+
+//ECS format
+
+import generateName from 'sillyname';
+
+var sillyname = generateName();
+console.log(`My name Is ${sillyname}`);
+
+
+import superheroes from 'superheroes';
+
+const name = superheroes.random();
+console.log(`I am ${name}!`);
+

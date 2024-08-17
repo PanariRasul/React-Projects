09a7dadfeadc12bd8a0b2fad0f8063fa9895c6f0
Rasul Yakub Panari <panarirasul@gmail.com>
2024-08-17 22:03:55 +0530

    using postman and body parser

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,33 @@
-document.querySelector("h1").innerHTML="Good Bye";
+import express from "express";
+
+const port = 8000;
+
+app.get("/", (res, req) => {
+    res.send("This Is Home Page!");
+});
+
+app.post("register",(req, res)=>{
+    res.sendStatus(200);
+});
+
+app.patch("user/rasul",(req, res)=>{
+    res.sendStatus(200);
+});
+
+app.put("user/rasul",(req, res)=>{
+    res.sendStatus(200);
+});
+
+app.delete("user/rasul", (req, res) =>{
+    res.sendStatus(200);
+});
+
+
+app.listen("3000", ()=>{
+    console.log("server up on port 3000")
+});
+
+
+app.listen(port,()=>{
+    console.log("server run on port 800");
+});

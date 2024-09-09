b3a0a6a2c9f502084268354135532402a2f51391
Rasul Yakub Panari <panarirasul@gmail.com>
2024-09-09 19:40:48 +0530

    update Keeper app

--- Keeper_App/src/components/Note.jsx
+++ Keeper_App/src/components/Note.jsx
@@ -2,9 +2,48 @@ import React from 'react';
 
 function Note() {
     return (
-        <div className="note">
-            <h1>Note Heading</h1>
-            <p>Note discription</p>
+        <div className='Container'>
+            <div className="note">
+                <h1>History of India</h1>
+                <p>This article is about the pre-1947 history of the Indian subcontinent. For the post-1947 history of India, see History of India (1947–present). For the post-1947 history of the Indian subcontinent, see South Asia § Contemporary era.
+                    "Ancient India" and "Indian history" redirect here. For outline, see Outline of South Asian history.</p>
+            </div>
+            <div className="note">
+                <h1>History of India</h1>
+                <p>This article is about the pre-1947 history of the Indian subcontinent. For the post-1947 history of India, see History of India (1947–present). For the post-1947 history of the Indian subcontinent, see South Asia § Contemporary era.
+                    "Ancient India" and "Indian history" redirect here. For outline, see Outline of South Asian history.</p>
+            </div>
+            <div className="note">
+                <h1>History of India</h1>
+                <p>This article is about the pre-1947 history of the Indian subcontinent. For the post-1947 history of India, see History of India (1947–present). For the post-1947 history of the Indian subcontinent, see South Asia § Contemporary era.
+                    "Ancient India" and "Indian history" redirect here. For outline, see Outline of South Asian history.</p>
+            </div>
+            <div className="note">
+                <h1>History of India</h1>
+                <p>This article is about the pre-1947 history of the Indian subcontinent. For the post-1947 history of India, see History of India (1947–present). For the post-1947 history of the Indian subcontinent, see South Asia § Contemporary era.
+                    "Ancient India" and "Indian history" redirect here. For outline, see Outline of South Asian history.</p>
+            </div>
+            <div className="note">
+                <h1>History of India</h1>
+                <p>This article is about the pre-1947 history of the Indian subcontinent. For the post-1947 history of India, see History of India (1947–present). For the post-1947 history of the Indian subcontinent, see South Asia § Contemporary era.
+                    "Ancient India" and "Indian history" redirect here. For outline, see Outline of South Asian history.</p>
+            </div>
+            <div className="note">
+                <h1>History of India</h1>
+                <p>This article is about the pre-1947 history of the Indian subcontinent. For the post-1947 history of India, see History of India (1947–present). For the post-1947 history of the Indian subcontinent, see South Asia § Contemporary era.
+                    "Ancient India" and "Indian history" redirect here. For outline, see Outline of South Asian history.</p>
+            </div>
+            <div className="note">
+                <h1>History of India</h1>
+                <p>This article is about the pre-1947 history of the Indian subcontinent. For the post-1947 history of India, see History of India (1947–present). For the post-1947 history of the Indian subcontinent, see South Asia § Contemporary era.
+                    "Ancient India" and "Indian history" redirect here. For outline, see Outline of South Asian history.</p>
+            </div>
+            <div className="note">
+                <h1>History of India</h1>
+                <p>This article is about the pre-1947 history of the Indian subcontinent. For the post-1947 history of India, see History of India (1947–present). For the post-1947 history of the Indian subcontinent, see South Asia § Contemporary era.
+                    "Ancient India" and "Indian history" redirect here. For outline, see Outline of South Asian history.</p>
+            </div>
+
         </div>
     );
 };

ec1ededbcacf8ecf593f9dfac20a0a05d73aa089
Rasul Yakub Panari <panarirasul@gmail.com>
2024-10-10 20:22:29 +0530

    note keeper

--- note_keeper_app/src/Components/CreateNote.jsx
+++ note_keeper_app/src/Components/CreateNote.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import AddIcon from '@mui/icons-material/Add';
 
 
 const CreateNote = () => {
+
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    });
+
+    const InputEvents = (event) => {
+        // Normal method
+        // const value = event.target.value;
+        // const name = event.target.name;
+
+        //object destructring
+
+        const { name, value } = event.target;
+
+
+        setNote((prevData) => {
+            return {
+                ...prevData,
+                [name]: value,
+            };
+        });
+
+    };
 
     const call = () => {
         return (alert("i got clicked"))
     };
 
     return <div className="createNote">
-        <input type="text" placeholder="Title" />
-        <textarea rows="3" cols="30" placeholder="Enter Note..."></textarea>
+        <input type="text" value={note.title} onChange={InputEvents} name="title" placeholder="Title" />
+        <textarea rows="3" cols="30" value={note.content} onChange={InputEvents} name="content" placeholder="Enter Note..."></textarea>
         <button className="btn" onClick={call}>
             <AddIcon className="addIcon" />
         </button>
